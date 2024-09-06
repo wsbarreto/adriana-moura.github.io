620135480c799460b58e2244f6f0e6ac1b9d1842
Willian Barreto <ws.barreto@hotmail.com>
2024-09-06 01:50:40 -0300

    novo teste

--- js/email.js
+++ js/email.js
@@ -1,5 +1,5 @@
 $.getJSON('https://wsbarreto.github.io/adriana-moura.github.io/appsettings.json', function(data) {
-    // code fragment
+    emailjs.init(data.PUBLICK_KEY); // Substitua com sua chave pública
     // the form id is myForm
     document.getElementById('form-contato').addEventListener('submit', function(event, resp) {
         event.preventDefault(); // prevent reload
